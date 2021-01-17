ab4facb1c862f133fd537ed407f84a5dde5f67f2
Ferda Nahit Fidanci <fnfidanci@gmail.com>
2021-01-17 04:14:15 +0300

    Home page added.

---
+++ lib/theme/styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:uber_eats_ui/theme/colors.dart';
+
+TextStyle customParagraph = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle customContent = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle customTitle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  height: 1.3,
+);
+
+BoxDecoration customIconContainerStyle = BoxDecoration(
+  color: textFieldColor,
+  borderRadius: BorderRadius.circular(2),
+);
